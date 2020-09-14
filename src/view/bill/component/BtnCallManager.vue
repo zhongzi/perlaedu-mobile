45f8556e88412b31665fe164997953b241cbf6e4
zhongzi <zhongzigm@gmail.com>
2020-09-14 15:49:30 +0800

    Fix 获取合伙人电话号码

--- src/view/bill/component/BtnCallManager.vue
+++ src/view/bill/component/BtnCallManager.vue
@@ -48,6 +48,7 @@ export default class Home extends Mixins(SyncMixin) {
       query: {
         merchant_id: this.merchantId,
         openid: this.openid,
+        extras: "phone",
       },
     });
   }
